c3a94978b7c6bb0e0b19915f0ba24a9eb2b0bec5
efraitor_ <efraitor.contact@gmail.com>
2024-05-27 11:57:52 +0200

    PauseMenu
    PauseMenu

---
+++ Assets/Code/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Para cambiar de escenas
+
+public class PauseMenu : MonoBehaviour
+{
+    //Variables para saber las escenas a las que queremos ir desde este menú
+    public string levelSelect, mainMenu;
+    //Variable para conocer cuando el juego está pausado o no
+    public bool isPaused;
+    //Referencia al GO del menú de pausa
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si pulsamos el botón de Intro
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("ButtonStart"))
+            //Pausamos el juego
+            PauseUnpause();
+        if (isPaused)
+            Input.GetButtonDown("ButtonA");
+    }
+
+    //Método para pausar o continuar el juego
+    public void PauseUnpause()
+    {
+        //Si la variable pausa es verdadera (el juego está pausado)
+        if (isPaused)
+        {
+            //Entonces el juego continuará
+            isPaused = false;
+            //Desactivamos el panel de pausa
+            pauseMenu.SetActive(false);
+            //Reanudamos el tiempo de juego
+            Time.timeScale = 1f;
+        }
+        //Si el juego no estaba pausado
+        else
+        {
+            //Entonces el juego se pondrá en pausa
+            isPaused = true;
+            //Activamos el panel de pausa
+            pauseMenu.SetActive(true);
+            //Pausamos el tiempo de juego
+            Time.timeScale = 0f;
+        }
+    }
+
+    //Método para el botón Resume
+    public void Resume()
+    {
+        //Pausamos el juego
+        PauseUnpause();
+    }
+
+    //Método para el botón LevelSelect
+    public void LevelSelect()
+    {
+        //Para ir a la escena LevelSelect
+        SceneManager.LoadScene(levelSelect);
+        //Reanudamos el tiempo de juego
+        Time.timeScale = 1f;
+    }
+
+    //Método para el botón MainMenu
+    public void MainMenu()
+    {
+        //Para ir a la escena MainMenu
+        SceneManager.LoadScene(mainMenu);
+        //Reanudamos el tiempo de juego
+        Time.timeScale = 1f;
+    }
+}
